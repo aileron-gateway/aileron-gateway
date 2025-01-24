355575aabcb6ec844f158fe37118c49466f75a82
k-tsurumaki <k-tsurumaki@nri.co.jp>
2025-01-24 13:37:35 +0900

    add example proxy-http1 test
    Signed-off-by: k-tsurumaki <k-tsurumaki@nri.co.jp>

---
+++ test/example/proxy_http1_test.go
@@ -0,0 +1,86 @@
+//go:build example
+// +build example
+
+package example_test
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"net/http"
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/aileron-gateway/aileron-gateway/kernel/testutil"
+)
+
+func MoveToRootDirectory(t *testing.T, targetDir string) {
+	err := os.Chdir(targetDir)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+}
+
+func TestProxyHttp1(t *testing.T) {
+
+	targetDir := "./../.."
+	MoveToRootDirectory(t, targetDir)
+
+	env := []string{}
+	config := []string{"./_example/proxy-http1/config-http1.yaml"}
+	entrypoint := getEntrypointRunner(t, env, config)
+	certFilePath := "./_example/proxy-http1/pki/cert.pem"
+
+	ctx, cancel := context.WithCancel(context.Background())
+	timer := time.AfterFunc(5*time.Second, cancel)
+
+	// Read the certificate
+	pem, err := os.ReadFile(certFilePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and start a http server
+	cmd := exec.Command("go", "run", "./_example/proxy-http1/server.go")
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	// Wait until the server finishes after all tests are done
+	defer func() {
+		cmd.Process.Kill()
+		cmd.Wait()
+	}()
+
+	// Wait until the server starts completely
+	time.Sleep(3 * time.Second)
+
+	// Create a new certificate pool and add the certificate to the pool
+	pool := x509.NewCertPool()
+	pool.AppendCertsFromPEM(pem)
+
+	var resp *http.Response
+	transport := &http.Transport{
+		ForceAttemptHTTP2: false,
+		TLSClientConfig: &tls.Config{
+			RootCAs: pool,
+		},
+	}
+
+	go func() {
+		req, _ := http.NewRequest(http.MethodGet, "https://localhost:8443/test", nil)
+		resp, err = transport.RoundTrip(req)
+		timer.Stop()
+		cancel()
+	}()
+
+	if err := entrypoint.Run(ctx); err != nil {
+		t.Error(err)
+	}
+
+	testutil.Diff(t, nil, err)
+	testutil.Diff(t, http.StatusOK, resp.StatusCode)
+}
