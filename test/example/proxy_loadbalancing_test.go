591b4e95b5c7d338ac81b02d1ca915953dea5694
k-tsurumaki <k-tsurumaki@nri.co.jp>
2025-01-29 12:47:41 +0900

    add exmaple proxy-loadbalancing test
    Signed-off-by: k-tsurumaki <k-tsurumaki@nri.co.jp>

---
+++ test/example/proxy_loadbalancing_test.go
@@ -0,0 +1,94 @@
+//go:build example
+// +build example
+
+package example_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"testing"
+	"time"
+	"sync"
+
+	"github.com/aileron-gateway/aileron-gateway/kernel/testutil"
+)
+
+func runServers(t *testing.T, testCtx context.Context) {
+	addrs := []string{
+		":8001",
+		":8002",
+		":8003",
+		":8004",
+		":8005",
+	}
+
+	wg := sync.WaitGroup{}
+	servers := make([]*http.Server, len(addrs))
+
+	for i, addr := range addrs {
+		wg.Add(1)
+		go func(i int, a string) {
+			defer wg.Done()
+			mux := &http.ServeMux{}
+			mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, "Hello! %s from %s\n", r.RemoteAddr, a)
+			})
+			svr := &http.Server{
+				Addr:    addr,
+				Handler: mux,
+			}
+
+			servers[i] = svr
+
+			log.Println("Server listens at", a)
+			if err := svr.ListenAndServe(); err != nil {
+				t.Fatal(err)
+			}
+		}(i, addr)
+	}
+
+	wg.Wait()
+
+	<-testCtx.Done()
+
+	for _, svr := range servers {
+		if err := svr.Shutdown(context.Background()); err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func TestProxyLoadBalancing(t *testing.T) {
+	targetDir := "./../.."
+	changeDirectory(t, targetDir)
+
+	env := []string{}
+	config := []string{"./_example/proxy-loadbalancing/config-direct-hash.yaml"}
+	entrypoint := getEntrypointRunner(t, env, config)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	timer := time.AfterFunc(5*time.Second, cancel)
+
+	go runServers(t, ctx)
+
+	time.Sleep(1 * time.Second)
+
+	var resp *http.Response
+	var err error
+
+	go func() {
+		req, _ := http.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+		resp, err = http.DefaultTransport.RoundTrip(req)
+		timer.Stop()
+		cancel()
+	}()
+
+	if err := entrypoint.Run(ctx); err != nil {
+		t.Error(err)
+	}
+
+	testutil.Diff(t, nil, err)
+	testutil.Diff(t, http.StatusOK, resp.StatusCode)
+}
