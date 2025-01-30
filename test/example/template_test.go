b0267a3562b4345a7cc845bc859ffefbb1fc7b85
k-tsurumaki <k-tsurumaki@nri.co.jp>
2025-01-30 14:57:09 +0900

    add example template test
    Signed-off-by: k-tsurumaki <k-tsurumaki@nri.co.jp>

---
+++ test/example/template_test.go
@@ -0,0 +1,43 @@
+//go:build example
+// +build example
+
+package example_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/aileron-gateway/aileron-gateway/kernel/testutil"
+)
+
+func TestStaticServer(t *testing.T) {
+
+	targetDir := "./../.."
+	changeDirectory(t, targetDir)
+
+	env := []string{}
+	config := []string{"./_example/template/"}
+	entrypoint := getEntrypointRunner(t, env, config)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	timer := time.AfterFunc(5*time.Second, cancel)
+
+	var resp *http.Response
+	var err error
+	go func() {
+		req, _ := http.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+		req.Header.Set("Accept", "text/plain")
+		resp, err = http.DefaultTransport.RoundTrip(req)
+		timer.Stop()
+		cancel()
+	}()
+
+	if err := entrypoint.Run(ctx); err != nil {
+		t.Error(err)
+	}
+
+	testutil.Diff(t, nil, err)
+	testutil.Diff(t, http.StatusInternalServerError, resp.StatusCode)
+}
