20e32b5d629ee20edee17ecb7f02a5d2dd643b31
k-tsurumaki <k-tsurumaki@nri.co.jp>
2025-02-03 13:20:31 +0900

    add example proxy-grpc test
    Signed-off-by: k-tsurumaki <k-tsurumaki@nri.co.jp>

---
+++ test/example/proxy_grpc_test.go
@@ -0,0 +1,183 @@
+//go:build example
+
+// + build example
+
+package example_test
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/aileron-gateway/aileron-gateway/kernel/testutil"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/examples/data"
+	"google.golang.org/protobuf/proto"
+
+	pb "google.golang.org/grpc/examples/route_guide/routeguide"
+)
+
+var (
+	// for server
+	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile   = flag.String("cert_file", "", "The TLS cert file")
+	keyFile    = flag.String("key_file", "", "The TLS key file")
+	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
+	port       = flag.Int("port", 50051, "The server port")
+
+	// for client
+	caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
+	serverAddr         = flag.String("addr", "localhost:50000", "The server address in the format of host:port")
+	serverHostOverride = flag.String("server_host_override", "x.test.example.com", "The server name used to verify the hostname returned by the TLS handshake")
+)
+
+type routeGuideServer struct {
+	pb.UnimplementedRouteGuideServer
+	savedFeatures []*pb.Feature // read-only after initialized
+
+	mu         sync.Mutex // protects routeNotes
+	routeNotes map[string][]*pb.RouteNote
+}
+
+// GetFeature returns the feature at the given point.
+func (s *routeGuideServer) GetFeature(_ context.Context, point *pb.Point) (*pb.Feature, error) {
+	for _, feature := range s.savedFeatures {
+		if proto.Equal(feature.Location, point) {
+			return feature, nil
+		}
+	}
+	// No feature was found, return an unnamed feature
+	return &pb.Feature{Location: point}, nil
+}
+
+// loadFeatures loads features from a JSON file.
+func (s *routeGuideServer) loadFeatures(filePath string) {
+	var data []byte
+	if filePath != "" {
+		var err error
+		data, err = os.ReadFile(filePath)
+		if err != nil {
+			log.Fatalf("Failed to load default features: %v", err)
+		}
+	} else {
+		data = exampleData
+	}
+	if err := json.Unmarshal(data, &s.savedFeatures); err != nil {
+		log.Fatalf("Failed to load default features: %v", err)
+	}
+}
+
+func serialize(point *pb.Point) string {
+	return fmt.Sprintf("%d %d", point.Latitude, point.Longitude)
+}
+
+func newServer() *routeGuideServer {
+	s := &routeGuideServer{routeNotes: make(map[string][]*pb.RouteNote)}
+	s.loadFeatures(*jsonDBFile)
+	return s
+}
+
+func runServer(t *testing.T, testCtx context.Context) {
+
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wg := sync.WaitGroup{}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterRouteGuideServer(grpcServer, newServer())
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		if err := grpcServer.Serve(lis); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	wg.Wait()
+
+	<-testCtx.Done()
+
+	grpcServer.GracefulStop()
+	if err := lis.Close(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestProxyGrpc(t *testing.T) {
+
+	targetDir := "./../.."
+	changeDirectory(t, targetDir)
+
+	env := []string{}
+	config := []string{"./_example/proxy-grpc/config-http-http.yaml"}
+	entrypoint := getEntrypointRunner(t, env, config)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	timer := time.AfterFunc(5*time.Second, cancel)
+
+	go runServer(t, ctx)
+	time.Sleep(1 * time.Second)
+
+	var opts []grpc.DialOption
+	if *tls {
+		if *caFile == "" {
+			*caFile = data.Path("x509/ca_cert.pem")
+		}
+		creds, err := credentials.NewClientTLSFromFile(*caFile, *serverHostOverride)
+		if err != nil {
+			log.Fatalf("Failed to create TLS credentials: %v", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	}
+
+	conn, err := grpc.NewClient("localhost:50000", opts...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	var feature *pb.Feature
+
+	go func() {
+		client := pb.NewRouteGuideClient(conn)
+		feature, err = client.GetFeature(ctx, &pb.Point{Latitude: 409146138, Longitude: -746188906})
+		if err != nil {
+			log.Printf("error getting feature: %v", err)
+		} else {
+			log.Printf("feature: %v", feature)
+		}
+		timer.Stop()
+		cancel()
+	}()
+
+	if err := entrypoint.Run(ctx); err != nil {
+		t.Error(err)
+	}
+
+	testutil.Diff(t, nil, err)
+	testutil.Diff(t, feature.GetName(), "Berkshire Valley Management Area Trail, Jefferson, NJ, USA")
+}
+
+var exampleData = []byte(`[{
+    "location": {
+        "latitude": 409146138,
+        "longitude": -746188906
+    },
+    "name": "Berkshire Valley Management Area Trail, Jefferson, NJ, USA"
+}]`)
