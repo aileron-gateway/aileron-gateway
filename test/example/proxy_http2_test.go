d5dace602c97f984176385aab563c8667dbf26df
k-tsurumaki <k-tsurumaki@nri.co.jp>
2025-02-05 10:55:11 +0900

    fix example proxy-http2 test
    Signed-off-by: k-tsurumaki <k-tsurumaki@nri.co.jp>

--- test/example/proxy_http2_test.go
+++ test/example/proxy_http2_test.go
@@ -7,69 +7,87 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"testing"
 	"time"
-	
+
 	"golang.org/x/net/http2"
 	"github.com/aileron-gateway/aileron-gateway/kernel/testutil"
 )
 
-func MoveToRootDirectory(t *testing.T, targetDir string) {
-	err := os.Chdir(targetDir)
-	if err != nil {
-		t.Fatal(err)
-		return
+const (
+	certFilePath = "./_example/proxy-http1/pki/cert.pem"
+	keyFilePath  = "./_example/proxy-http1/pki/key.pem"
+)
+
+func runHTTP2(t *testing.T, ctx context.Context) {
+	svr := &http.Server{
+		Addr:         ":10002",
+		Handler:      http.HandlerFunc(handler),
+	}
+
+	log.Println("HTTP 2 server listens on", svr.Addr)
+
+	go func() {
+		if err := svr.ListenAndServeTLS(certFilePath, keyFilePath); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	time.Sleep(1 * time.Second)
+
+	if err := svr.Shutdown(context.Background()); err != nil {
+		t.Error(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Method : %s\n", r.Method)
+	fmt.Fprintf(w, "Path : %s\n", r.URL.Path)
+	fmt.Fprintf(w, "HTTP : %d.%d\n", r.ProtoMajor, r.ProtoMinor)
+	fmt.Fprintf(w, "Header:\n")
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "  %s: %+v\n", k, v)
 	}
 }
 
 func TestProxyHttp2(t *testing.T) {
 
 	targetDir := "./../.."
-	MoveToRootDirectory(t, targetDir)
+	changeDirectory(t, targetDir)
 
 	env := []string{}
 	config := []string{"./_example/proxy-http2/config-http2.yaml"}
 	entrypoint := getEntrypointRunner(t, env, config)
-	certFilePath := "./_example/proxy-http2/pki/cert.pem"
 
 	ctx, cancel := context.WithCancel(context.Background())
 	timer := time.AfterFunc(5*time.Second, cancel)
 
-	// Read the certificate
 	pem, err := os.ReadFile(certFilePath)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 
-	// Create and start a http server
-	cmd := exec.Command("go", "run", "./_example/proxy-http2/server.go")
-	if err := cmd.Start(); err != nil {
-		t.Fatal(err)
-	}
+	go runHTTP2(t, ctx)
 
-	// Wait until the server finishes after all tests are done
-	defer func() {
-		cmd.Process.Kill()
-		cmd.Wait()
-	}()
+	time.Sleep(1 * time.Second)
 
-	// Wait until the server starts completely
-	time.Sleep(3 * time.Second)
-
-	// Create a new certificate pool and add the certificate to the pool
 	pool := x509.NewCertPool()
 	pool.AppendCertsFromPEM(pem)
 
-	var resp *http.Response
 	transport := &http2.Transport{
 		TLSClientConfig: &tls.Config{
 			RootCAs: pool,
 		},
 	}
-
+	
+	var resp *http.Response
+	
 	go func() {
 		req, _ := http.NewRequest(http.MethodGet, "https://localhost:8443/test", nil)
 		resp, err = transport.RoundTrip(req)
