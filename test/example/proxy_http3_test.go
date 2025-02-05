53461f30677ef76a6459537c6e80dc218bbcfd97
k-tsurumaki <k-tsurumaki@nri.co.jp>
2025-02-05 11:13:07 +0900

    fix example proxy-http3 test
    Signed-off-by: k-tsurumaki <k-tsurumaki@nri.co.jp>

--- test/example/proxy_http3_test.go
+++ test/example/proxy_http3_test.go
@@ -1,5 +1,5 @@
 //go:build example
-//+build example
+// +build example
 
 package example_test
 
@@ -7,64 +7,87 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"testing"
 	"time"
-	
+
 	"github.com/quic-go/quic-go/http3"
 	"github.com/aileron-gateway/aileron-gateway/kernel/testutil"
 )
 
-func MoveToRootDirectory(t *testing.T, targetDir string) {
-	err := os.Chdir(targetDir)
-	if err != nil {
-		t.Fatal(err)
-		return
+const (
+	certFilePath = "./_example/proxy-http1/pki/cert.pem"
+	keyFilePath  = "./_example/proxy-http1/pki/key.pem"
+)
+
+func runHTTP3(t *testing.T, ctx context.Context) {
+	svr := &http3.Server{
+		Addr:         ":10003",
+		Handler:      http.HandlerFunc(handler),
+	}
+
+	log.Println("HTTP 3 server listens on", svr.Addr)
+
+	go func() {
+		if err := svr.ListenAndServeTLS(certFilePath, keyFilePath); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	time.Sleep(1 * time.Second)
+
+	if err := svr.Shutdown(context.Background()); err != nil {
+		t.Error(err)
 	}
 }
 
-func TestProxyHttp3(t *testing.T) {
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Method : %s\n", r.Method)
+	fmt.Fprintf(w, "Path : %s\n", r.URL.Path)
+	fmt.Fprintf(w, "HTTP : %d.%d\n", r.ProtoMajor, r.ProtoMinor)
+	fmt.Fprintf(w, "Header:\n")
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "  %s: %+v\n", k, v)
+	}
+}
+
+func TestProxyHttp1(t *testing.T) {
 
 	targetDir := "./../.."
-	MoveToRootDirectory(t, targetDir)
+	changeDirectory(t, targetDir)
 
 	env := []string{}
 	config := []string{"./_example/proxy-http3/config-http3.yaml"}
 	entrypoint := getEntrypointRunner(t, env, config)
-	certFilePath := "./_example/proxy-http3/pki/cert.pem"
 
 	ctx, cancel := context.WithCancel(context.Background())
 	timer := time.AfterFunc(5*time.Second, cancel)
 
 	pem, err := os.ReadFile(certFilePath)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 
-	cmd := exec.Command("go", "run", "./_example/proxy-http3/server.go")
-	if err := cmd.Start(); err != nil {
-		t.Fatal(err)
-	}
+	go runHTTP3(t, ctx)
 
-	defer func() {
-		cmd.Process.Kill()
-		cmd.Wait()
-	}()
-
-	time.Sleep(3*time.Second)
+	time.Sleep(1 * time.Second)
 
 	pool := x509.NewCertPool()
 	pool.AppendCertsFromPEM(pem)
 
-	var resp *http.Response
 	transport := &http3.Transport{
 		TLSClientConfig: &tls.Config{
 			RootCAs: pool,
 		},
 	}
-
+	
+	var resp *http.Response
+	
 	go func() {
 		req, _ := http.NewRequest(http.MethodGet, "https://localhost:8443/test", nil)
 		resp, err = transport.RoundTrip(req)
