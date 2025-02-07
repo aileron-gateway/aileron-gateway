e3d206f58c2d71c574da1e7bc3f09864bb557161
k-tsurumaki <k-tsurumaki@nri.co.jp>
2025-02-07 17:15:32 +0900

    add example listen-unix-socket test
    Signed-off-by: k-tsurumaki <k-tsurumaki@nri.co.jp>

---
+++ test/example/listen_unix_socket_test.go
@@ -0,0 +1,52 @@
+//go:build example
+// +build example
+
+package example_test
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/aileron-gateway/aileron-gateway/kernel/testutil"
+)
+
+func TestUnixSocket(t *testing.T) {
+
+	wd, _ := os.Getwd()
+	defer changeDirectory(t, wd)
+	changeDirectory(t, "./../../")
+
+	env := []string{}
+	config := []string{"_example/listen-unix-socket/"}
+	entrypoint := getEntrypointRunner(t, env, config)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	dial := func(network, addr string) (net.Conn, error) {
+		return net.Dial("unix", "@gateway")
+	}
+
+	transport := &http.Transport{
+		Dial: dial,
+	}
+
+	var resp *http.Response
+	var err error
+	go func() {
+		req, _ := http.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+		req.Header.Set("Accept", "text/plain")
+		resp, err = transport.RoundTrip(req)
+		cancel()
+	}()
+
+	if err := entrypoint.Run(ctx); err != nil {
+		t.Error(err)
+	}
+
+	testutil.Diff(t, nil, err)
+	testutil.Diff(t, http.StatusOK, resp.StatusCode)
+}
