ec4233d192b456553d3bbd9939682aff20ebdf81
t-katsumura <t-katsumura@nri.co.jp>
2025-02-07 23:14:25 +0900

    improve websocket test
    Signed-off-by: t-katsumura <t-katsumura@nri.co.jp>

--- test/example/proxy_websocket_test.go
+++ test/example/proxy_websocket_test.go
@@ -6,9 +6,10 @@ package example_test
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"testing"
 	"time"
 
@@ -16,16 +17,15 @@ import (
 	"golang.org/x/net/websocket"
 )
 
-func runServer(t *testing.T, ctx context.Context) {
+func runWebSocketServer(t *testing.T, ctx context.Context) {
 	addr := "0.0.0.0:9999"
 	log.Println("WebSocket server listens at", addr)
 
-	dir := http.Dir("./")
-	http.Handle("/", http.FileServer(dir))
-	http.HandleFunc("/ws", ws())
-
+	mux := http.NewServeMux()
+	mux.Handle("/", ws())
 	svr := &http.Server{
-		Addr: addr,
+		Addr:    addr,
+		Handler: mux,
 	}
 
 	go func() {
@@ -34,10 +34,7 @@ func runServer(t *testing.T, ctx context.Context) {
 		}
 	}()
 
-	time.Sleep(time.Second * 1)
-
 	<-ctx.Done()
-
 	err := svr.Shutdown(context.Background())
 	if err != nil {
 		t.Error(err)
@@ -48,56 +45,34 @@ func ws() http.HandlerFunc {
 	return websocket.Handler(func(ws *websocket.Conn) {
 		defer ws.Close()
 
-		err := websocket.Message.Send(ws, "Hello!! This is a WebSocket server!!")
+		err := websocket.Message.Send(ws, "Hello!")
 		if err != nil {
 			panic(err)
 		}
 
 		done := make(chan struct{})
-
-		// Receive message from client.
-		go func() {
-			for {
-				msg := ""
-				err = websocket.Message.Receive(ws, &msg)
-				if err != nil {
-					log.Println(err)
-					close(done)
-					return
-				}
-				err := websocket.Message.Send(ws, fmt.Sprintf("Your message arrived: %s", msg))
-				if err != nil {
-					log.Println(err)
-					close(done)
-					return
-				}
-			}
-		}()
-
 		// Send message to client.
-		for {
-			select {
-			case <-ws.Request().Context().Done():
-				return
-			case <-done:
-				return
-			case <-time.After(time.Second):
-			}
-			err := websocket.Message.Send(ws, fmt.Sprintf("It's %s\n", time.Now().Format(http.TimeFormat)))
+		for i := 1; i <= 5; i++ {
+			err := websocket.Message.Send(ws, strconv.Itoa(i))
 			if err != nil {
 				log.Println(err)
 				close(done)
 				break
 			}
 		}
-
+		select {
+		case <-ws.Request().Context().Done():
+			return
+		case <-done:
+			return
+		}
 	}).ServeHTTP
 }
 
 func TestProxyWebsocket(t *testing.T) {
-
-	targetDir := "./../.."
-	changeDirectory(t, targetDir)
+	wd, _ := os.Getwd()
+	defer changeDirectory(t, wd)
+	changeDirectory(t, "./../../")
 
 	env := []string{}
 	config := []string{"./_example/proxy-sse/config.yaml"}
@@ -105,17 +80,27 @@ func TestProxyWebsocket(t *testing.T) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	timer := time.AfterFunc(5*time.Second, cancel)
+	go runWebSocketServer(t, ctx)
+	time.Sleep(1 * time.Second) // Wait for the server start up.
 
-	go runServer(t, ctx)
-
-	time.Sleep(1 * time.Second)
-
-	var resp *http.Response
 	var err error
-
+	var msg string
 	go func() {
-		req, _ := http.NewRequest(http.MethodGet, "http://localhost:8080/_example/proxy-websocket/", nil)
-		resp, err = http.DefaultTransport.RoundTrip(req)
+		time.Sleep(time.Second)
+		ws, dialErr := websocket.Dial("ws://localhost:8080", "", "http://localhost:8080")
+		if dialErr != nil {
+			panic(dialErr)
+			// t.Error(dialErr)
+		}
+		defer ws.Close()
+		for {
+			tmp := ""
+			if err := websocket.Message.Receive(ws, &tmp); err != nil {
+				t.Error(err)
+				break
+			}
+			msg += tmp
+		}
 		timer.Stop()
 		cancel()
 	}()
@@ -125,5 +110,5 @@ func TestProxyWebsocket(t *testing.T) {
 	}
 
 	testutil.Diff(t, nil, err)
-	testutil.Diff(t, http.StatusOK, resp.StatusCode)
+	testutil.Diff(t, "Hello!12345", msg)
 }
