51ba5eac091437969024d693f934ca3679ae5949
inadamas <inadamas@fsi.co.jp>
2025-03-13 10:20:39 +0900

    Add test case in middleware to handle requests with `Content-Length: -1` and `Transfer-Encoding: chunked`

--- test/integration/httplogger/httplogger_test.go
+++ test/integration/httplogger/httplogger_test.go
@@ -432,3 +432,122 @@ func TestMaskBodiesJSON_Tripperware(t *testing.T) {
 	testutil.Diff(t, true, strings.Contains(str, `\"test\":\"response\"`))
 
 }
+
+func TestContentLengthNegativeOne_Tripperware(t *testing.T) {
+	configs := []string{
+		testDataDir + "config-mask-bodies.yaml",
+	}
+
+	server := common.NewAPI()
+	err := app.LoadConfigFiles(server, configs)
+	testutil.DiffError(t, nil, nil, err)
+
+	tmp := os.Stdout
+	defer func() { os.Stdout = tmp }()
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	ref := &kernel.Reference{
+		APIVersion: "core/v1",
+		Kind:       "HTTPLogger",
+		Name:       "default",
+		Namespace:  "",
+	}
+	tr, err := api.ReferTypedObject[core.Tripperware](server, ref)
+	testutil.DiffError(t, nil, nil, err)
+	testHTTPLoggerTripperware(t, tr)
+
+	rt := tr.Tripperware(core.RoundTripperFunc(
+		func(r *http.Request) (*http.Response, error) {
+			body := []byte(`{"bar":"BAR","test":"response"}`)
+			return &http.Response{
+				StatusCode:       http.StatusOK,
+				ContentLength:    -1, // Content-Length: -1 に設定
+				TransferEncoding: []string{"chunked"},
+				Header: http.Header{
+					"Test":         {"ok"},
+					"Content-Type": {"application/json"},
+				},
+				Body: io.NopCloser(bytes.NewReader(body)),
+			}, nil
+		},
+	))
+
+	body := bytes.NewReader([]byte(`{"foo":"FOO","test":"request"}`))
+	req := httptest.NewRequest(http.MethodGet, "/test", body)
+	req.Header.Set("Content-Type", "application/json")
+	res, _ := rt.RoundTrip(req)
+	b, _ := io.ReadAll(res.Body)
+	testutil.Diff(t, http.StatusOK, res.StatusCode)
+	testutil.Diff(t, "ok", res.Header.Get("test"))
+	testutil.Diff(t, `{"bar":"BAR","test":"response"}`, string(b))
+
+	w.Close()
+	var buf bytes.Buffer
+	buf.ReadFrom(r)
+	str := buf.String()
+	t.Log(str, "\n")
+	testutil.Diff(t, true, strings.Contains(str, `\"foo\":\"##MASKED##\"`))
+	testutil.Diff(t, true, strings.Contains(str, `\"bar\":\"##MASKED##\"`))
+	testutil.Diff(t, true, strings.Contains(str, `\"test\":\"request\"`))
+	testutil.Diff(t, true, strings.Contains(str, `\"test\":\"response\"`))
+}
+
+func TestMaskBodies_Middleware_ContentLengthMinusOne(t *testing.T) {
+
+	configs := []string{
+		testDataDir + "config-mask-bodies.yaml",
+	}
+
+	server := common.NewAPI()
+	err := app.LoadConfigFiles(server, configs)
+	testutil.DiffError(t, nil, nil, err)
+
+	tmp := os.Stdout
+	defer func() { os.Stdout = tmp }()
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	ref := &kernel.Reference{
+		APIVersion: "core/v1",
+		Kind:       "HTTPLogger",
+		Name:       "default",
+		Namespace:  "",
+	}
+	m, err := api.ReferTypedObject[core.Middleware](server, ref)
+	testutil.DiffError(t, nil, nil, err)
+	testHTTPLoggerMiddleware(t, m)
+
+	h := m.Middleware(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			body := []byte(`{"bar":"BAR","test":"response"}`)
+			w.Header().Set("test", "ok")
+			w.Header().Set("Content-Type", "application/json")
+			w.Header().Set("Transfer-Encoding", "chunked")
+			w.Header().Set("Content-Length", "-1") // Content-Length: -1
+			w.WriteHeader(http.StatusOK)
+			w.Write(body)
+		}),
+	)
+
+	body := bytes.NewReader([]byte(`{"foo":"FOO","test":"request"}`))
+	req := httptest.NewRequest(http.MethodGet, "/test", body)
+	req.Header.Set("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	h.ServeHTTP(res, req)
+	b, _ := io.ReadAll(res.Body)
+	testutil.Diff(t, http.StatusOK, res.Result().StatusCode)
+	testutil.Diff(t, "ok", res.Header().Get("test"))
+	testutil.Diff(t, `{"bar":"BAR","test":"response"}`, string(b))
+
+	w.Close()
+	var buf bytes.Buffer
+	buf.ReadFrom(r)
+	str := buf.String()
+	t.Log(str, "\n")
+	testutil.Diff(t, true, strings.Contains(str, `\"foo\":\"##MASKED##\"`))
+	testutil.Diff(t, true, strings.Contains(str, `\"bar\":\"##MASKED##\"`))
+	testutil.Diff(t, true, strings.Contains(str, `\"test\":\"request\"`))
+	testutil.Diff(t, true, strings.Contains(str, `\"test\":\"response\"`))
+
+}
