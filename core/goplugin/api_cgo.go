47d35c730d671f0865297360bcc40c55016d2d69
t-katsumura <83070078+t-katsumura@users.noreply.github.com>
2025-07-31 18:27:36 +0900

    use zplugin package (#99)

--- core/goplugin/api_cgo.go
+++ core/goplugin/api_cgo.go
@@ -7,7 +7,6 @@ package goplugin
 
 import (
 	"cmp"
-	"plugin"
 
 	v1 "github.com/aileron-gateway/aileron-gateway/apis/core/v1"
 	"github.com/aileron-gateway/aileron-gateway/apis/kernel"
@@ -15,6 +14,7 @@ import (
 	"github.com/aileron-gateway/aileron-gateway/kernel/api"
 	"github.com/aileron-gateway/aileron-gateway/kernel/log"
 	utilhttp "github.com/aileron-gateway/aileron-gateway/util/http"
+	"github.com/aileron-projects/go/zplugin"
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
@@ -44,62 +44,24 @@ type API struct {
 	*api.BaseResource
 }
 
-// testPlugin is the plugin only used for testing.
-var testPlugin any
-
-// testOpen is the plugin open function only used for testing.
-var testOpen func(path string) (*plugin.Plugin, error)
-
 func (*API) Create(a api.API[*api.Request, *api.Response], msg protoreflect.ProtoMessage) (any, error) {
 	c := msg.(*v1.GoPlugin)
 
 	lg := log.DefaultOr(c.Metadata.Logger)
 	eh := utilhttp.GlobalErrorHandler(cmp.Or(c.Metadata.ErrorHandler, utilhttp.DefaultErrorHandlerName))
 
-	p, err := plugin.Open(c.Spec.PluginPath)
-	if testOpen != nil { // Skip error when testing.
-		p, err = testOpen(c.Spec.PluginPath)
-	}
+	s, err := zplugin.Lookup(c.Spec.PluginPath, c.Spec.SymbolName)
 	if err != nil {
 		return nil, core.ErrCoreGenCreateObject.WithStack(err, map[string]any{"kind": kind})
 	}
-
-	s, err := p.Lookup(c.Spec.SymbolName)
-	if testPlugin != nil { // Replace plugin for testing.
-		s = testPlugin
-		err = nil
-	}
-	if err != nil {
-		return nil, core.ErrCoreGenCreateObject.WithStack(err, map[string]any{"kind": kind})
-	}
-
-	if i, ok := s.(InitializerA); ok {
-		if err := i.Init(); err != nil {
-			return nil, core.ErrCoreGenCreateObject.WithStack(err, map[string]any{"kind": kind})
-		}
-	}
-	if i, ok := s.(InitializerB); ok {
-		if err := i.Init(lg); err != nil {
-			return nil, core.ErrCoreGenCreateObject.WithStack(err, map[string]any{"kind": kind})
-		}
-	}
-	if i, ok := s.(InitializerC); ok {
+	if i, ok := s.(Initializer); ok {
 		if err := i.Init(lg, eh); err != nil {
 			return nil, core.ErrCoreGenCreateObject.WithStack(err, map[string]any{"kind": kind})
 		}
 	}
-
 	return s, nil
 }
 
-type InitializerA interface {
-	Init() error
-}
-
-type InitializerB interface {
-	Init(log.Logger) error
-}
-
-type InitializerC interface {
+type Initializer interface {
 	Init(log.Logger, core.ErrorHandler) error
 }
