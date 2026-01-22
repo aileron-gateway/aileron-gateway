945c8da315135480fe0811c4f9c50e286e904f0b
RyosukeDTomita <d310151c2@gmail.com>
2026-01-22 17:22:10 +0900

    feat: use nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Go 1.24.0 development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go_1_25
+            gopls
+            gotools
+            go-tools
+          ];
+          shellHook = ''
+            export GOTOOLCHAIN=local
+          '';
+        };
+      }
+    );
+}
